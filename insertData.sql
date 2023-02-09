67ba9b702eb4ea22df8d3362150418b2ccf557e4
kiwamoto <iwamoto_kenji83@yahoo.co.jp>
2023-02-09 13:53:55 +0900

    データベース操作

--- insertData.sql
+++ insertData.sql
@@ -1,8 +1,8 @@
 insert into articles (title, contents, username, nice, created__at) values
-    ('firstPost', 'This is my first blog', 'saki', 2, now());
+    ('firstPost', 'This is my first blog', 'taro', 2, now());
 
 insert into articles (title, contents, username, nice) values
-    ('2nd', 'Second blog post', 'saki', 4);
+    ('2nd', 'Second blog post', 'taro', 4);
 
 insert into comments (article_id, message, created_at) values
     (1, '1st comment yeah', now());
