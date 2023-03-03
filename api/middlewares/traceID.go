aef42efba2a247f3cda3b3518416f8da87cfd928
kiwamoto <iwamoto_kenji83@yahoo.co.jp>
2023-03-03 13:08:49 +0900

    ロギング修正

--- api/middlewares/traceID.go
+++ api/middlewares/traceID.go
@@ -10,12 +10,14 @@ var (
 	mu    sync.Mutex
 )
 
+type traceIDKey struct{}
+
 func SetTraceID(ctx context.Context, traceID int) context.Context {
-	return context.WithValue(ctx, "traceID", traceID)
+	return context.WithValue(ctx, traceIDKey{}, traceID)
 }
 
 func GetTraceID(ctx context.Context) int {
-	id := ctx.Value("traceID")
+	id := ctx.Value(traceIDKey{})
 
 	if idInt, ok := id.(int); ok {
 		return idInt
